7107c083f902d560ff19e223c92b1eeeae2f2267
ITExcellence <itexcellence.official@gmail.com>
2026-09-13 16:13:54 +0600

    django templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,33 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h1> My Blog</h1>")
+    posts = [
+        {
+        "title": "My First Post",
+        "author": "Alice",
+        },
+        {
+        "title": "Learning Django",
+        "author": "Bob",
+        },
+        {
+        "title": "Why Python Is Fun",
+        "author": "Charlie",
+        },
+    ]
+    
+    context = {
+        "posts": posts,
+    }
+
+    
+    return render(request, "blog/home.html", context)
+
+# contact
+def contact(request):
+    return render(request, "blog/contact.html")
+
+# about-us
+def about(request):
+    return render(request, "blog/about.html")
+
